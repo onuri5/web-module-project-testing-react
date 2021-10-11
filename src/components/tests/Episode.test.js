9d686c20cf9c3ed73a50c1b0ade75b45e0baaf32
onuri5 <jomibaofficial@gmail.com>
2021-10-11 18:30:24 -0500

    done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -8,6 +8,16 @@ const testEpisode = {
     image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
     season: 1,
     number: 1,
+    summary: "test",
+    runtime: 1
+}
+
+const testEpisode2 = {
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
     summary: "",
     runtime: 1
 }
@@ -17,15 +27,26 @@ const testEpisodeWithoutImage = {
 }
 
 test("renders without error", () => {
-
+    render(<Episode episode={testEpisode}/>);
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", ()=>{
+    render(<Episode episode={testEpisode}/>);
+
+    const summary = screen.getByText('test');
+
+    expect(summary).toBeInTheDocument();
 });
 
 test("renders default image when image is not defined", ()=>{
+    render(<Episode episode={testEpisode2}/>);
+
+    const img = screen.getByRole('img');
+
+    testEpisode2.image = './stranger_things.png'
     
+    expect(img).toHaveAttribute('src', './stranger_things.png');
+    expect(img).toHaveAttribute('alt', './stranger_things.png');
 })
 
 //Tasks
